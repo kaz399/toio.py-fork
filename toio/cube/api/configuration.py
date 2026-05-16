fcec47cf6235e891af0788eb1bad91a9c5faac50
YABE.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2026-05-16 11:01:25 +0900

    Add savable configuration API

--- toio/cube/api/configuration.py
+++ toio/cube/api/configuration.py
@@ -91,6 +91,23 @@ class SetDoubleTapDetectionTimeInterval(CubeCommand):
         return bytes((self._payload_id, 0x00, self.threshold))
 
 
+class ResetConfiguration(CubeCommand):
+    """
+    Reset configuration command
+
+    References:
+        https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E4%BF%9D%E5%AD%98%E3%81%97%E3%81%9F%E8%A8%AD%E5%AE%9A%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96
+    """
+
+    _payload_id = 0x0F
+
+    def __init__(self) -> None:
+        pass
+
+    def __bytes__(self) -> bytes:
+        return bytes((self._payload_id, 0x00))
+
+
 class NotificationCondition(IntEnum):
     """
     Notification conditions of ID notification
@@ -390,6 +407,33 @@ class GetCurrentConnectionIntervalValue(CubeCommand):
         return bytes((self._payload_id, 0x00))
 
 
+class SpeakerMute(IntEnum):
+    """
+    Speaker mute settings
+    """
+
+    Unmute = 0x00
+    MuteAll = 0x01
+    MuteSystemOnly = 0x02
+
+
+class SetSpeakerMute(CubeCommand):
+    """
+    Speaker mute setting command
+
+    References:
+        https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E3%82%B9%E3%83%94%E3%83%BC%E3%82%AB%E3%83%BC%E6%B6%88%E9%9F%B3%E3%81%AE%E8%A8%AD%E5%AE%9A
+    """
+
+    _payload_id = 0x33
+
+    def __init__(self, mute: SpeakerMute) -> None:
+        self.mute = mute
+
+    def __bytes__(self) -> bytes:
+        return bytes((self._payload_id, 0x00, self.mute))
+
+
 class RequestRemotePowerOff(CubeCommand):
     """
     Request remote power off command
@@ -436,6 +480,35 @@ class ProtocolVersion(CubeResponse):
                 self._major = int(version_numbers[0])
                 self._minor = int(version_numbers[1])
                 self._patch = int(version_numbers[2])
+        else:
+            raise TypeError("wrong payload")
+
+    def __str__(self) -> str:
+        return pprint.pformat(vars(self))
+
+
+class ResponseConfigurationReset(CubeResponse):
+    """
+    Configuration reset response
+
+    Attributes:
+        result (bool): Result of the command
+
+    References:
+        https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E4%BF%9D%E5%AD%98%E3%81%97%E3%81%9F%E8%A8%AD%E5%AE%9A%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96%E3%81%AE%E5%BF%9C%E7%AD%94
+    """
+
+    _payload_id = 0x8F
+    _converter = struct.Struct("<BBB")
+
+    @staticmethod
+    def is_myself(payload: GattReadData) -> bool:
+        return payload[0] == ResponseConfigurationReset._payload_id
+
+    def __init__(self, payload: GattReadData):
+        if ResponseConfigurationReset.is_myself(payload):
+            _, _, result = self._converter.unpack_from(payload)
+            self.result = result == 0x00
         else:
             raise TypeError("wrong payload")
 
@@ -680,6 +753,35 @@ class ResponseGettingCurrentConnectionInterval(CubeResponse):
         return pprint.pformat(vars(self))
 
 
+class ResponseSpeakerMuteSettings(CubeResponse):
+    """
+    Speaker mute setting response
+
+    Attributes:
+        result (bool): Result of the command
+
+    References:
+        https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E3%82%B9%E3%83%94%E3%83%BC%E3%82%AB%E3%83%BC%E6%B6%88%E9%9F%B3%E3%81%AE%E8%A8%AD%E5%AE%9A%E3%81%AE%E5%BF%9C%E7%AD%94
+    """
+
+    _payload_id = 0xB3
+    _converter = struct.Struct("<BBB")
+
+    @staticmethod
+    def is_myself(payload: GattReadData) -> bool:
+        return payload[0] == ResponseSpeakerMuteSettings._payload_id
+
+    def __init__(self, payload: GattReadData):
+        if ResponseSpeakerMuteSettings.is_myself(payload):
+            _, _, result = self._converter.unpack_from(payload)
+            self.result = result == 0x00
+        else:
+            raise TypeError("wrong payload")
+
+    def __str__(self) -> str:
+        return pprint.pformat(vars(self))
+
+
 class ResponseRemotePowerOff(CubeResponse):
     """
     Response of remote power off request
@@ -711,6 +813,7 @@ class ResponseRemotePowerOff(CubeResponse):
 
 ConfigurationResponseType: TypeAlias = Union[
     ProtocolVersion,
+    ResponseConfigurationReset,
     ResponseIdNotificationSettings,
     ResponseIdMissedNotificationSettings,
     ResponseMagneticSensorSettings,
@@ -719,6 +822,7 @@ ConfigurationResponseType: TypeAlias = Union[
     ResponseConnectionIntervalRequest,
     ResponseGettingRequestedConnectionInterval,
     ResponseGettingCurrentConnectionInterval,
+    ResponseSpeakerMuteSettings,
     ResponseRemotePowerOff,
 ]
 """
@@ -738,6 +842,8 @@ class Configuration(CubeCharacteristic):
     def is_my_data(payload: GattReadData) -> Optional[ConfigurationResponseType]:
         if ProtocolVersion.is_myself(payload):
             return ProtocolVersion(payload)
+        elif ResponseConfigurationReset.is_myself(payload):
+            return ResponseConfigurationReset(payload)
         elif ResponseIdNotificationSettings.is_myself(payload):
             return ResponseIdNotificationSettings(payload)
         elif ResponseIdMissedNotificationSettings.is_myself(payload):
@@ -754,6 +860,8 @@ class Configuration(CubeCharacteristic):
             return ResponseGettingRequestedConnectionInterval(payload)
         elif ResponseGettingCurrentConnectionInterval.is_myself(payload):
             return ResponseGettingCurrentConnectionInterval(payload)
+        elif ResponseSpeakerMuteSettings.is_myself(payload):
+            return ResponseSpeakerMuteSettings(payload)
         elif ResponseRemotePowerOff.is_myself(payload):
             return ResponseRemotePowerOff(payload)
         else:
@@ -986,4 +1094,39 @@ class Configuration(CubeCharacteristic):
             https://toio.github.io/toio-spec/docs/ble_configuration/#%E3%83%AA%E3%83%A2%E3%83%BC%E3%83%88%E9%9B%BB%E6%BA%90%E3%82%AA%E3%83%95%E3%81%AE%E8%A6%81%E6%B1%82
         """
         command = RequestRemotePowerOff(time_s)
+        await self._write(bytes(command))
+
+    async def set_speaker_mute(self, mute: SpeakerMute) -> None:
+        """
+        Send speaker mute setting command
+
+        This function DO NOT return response payload.
+        Receive the result by notification.
+
+        Note:
+            To write this setting, the function button must be pressed.
+
+        Args:
+            mute (SpeakerMute): Mute setting
+
+        References:
+            https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E3%82%B9%E3%83%94%E3%83%BC%E3%82%AB%E3%83%BC%E6%B6%88%E9%9F%B3%E3%81%AE%E8%A8%AD%E5%AE%9A
+        """
+        command = SetSpeakerMute(mute)
+        await self._write(bytes(command))
+
+    async def reset_configuration(self) -> None:
+        """
+        Send configuration reset command
+
+        This function DO NOT return response payload.
+        Receive the result by notification.
+
+        Note:
+            To write this setting, the function button must be pressed.
+
+        References:
+            https://toio.github.io/toio-spec/docs/ble_savable_configuration/#%E4%BF%9D%E5%AD%98%E3%81%97%E3%81%9F%E8%A8%AD%E5%AE%9A%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96
+        """
+        command = ResetConfiguration()
         await self._write(bytes(command))
