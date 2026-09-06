3a07f420a0e4bccdde3e594a14de6db28ab9b969
YABE.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2026-09-06 20:12:57 +0900

    docs: correct scanner return type in docstrings
    UniversalBleScanner returns List[CubeInfo]. The docstrings still named
    List[Tuple[BLEDevice, AdvertisementData]], which was the return type
    before CubeInfo was introduced.

--- toio/scanner/ble.py
+++ toio/scanner/ble.py
@@ -74,7 +74,7 @@ class UniversalBleScanner(ScannerInterface):
             timeout (float, optional): Scan timeout. Defaults to DEFAULT_SCAN_TIMEOUT.
 
         Returns:
-            List[Tuple[BLEDevice, AdvertisementData]]: List of found cubes.
+            List[CubeInfo]: List of found cubes.
         """
         return await self._scan(num=num, sort=sort, timeout=timeout)
 
@@ -96,7 +96,7 @@ class UniversalBleScanner(ScannerInterface):
             timeout (float, optional): Scan timeout. Defaults to DEFAULT_SCAN_TIMEOUT.
 
         Returns:
-            List[Tuple[BLEDevice, AdvertisementData]]: List of found cubes.
+            List[CubeInfo]: List of found cubes.
         """
         return await self._scan(cube_id=cube_id, sort=sort, timeout=timeout)
 
@@ -118,7 +118,7 @@ class UniversalBleScanner(ScannerInterface):
             timeout (float, optional): Scan timeout. Defaults to DEFAULT_SCAN_TIMEOUT.
 
         Returns:
-            List[Tuple[BLEDevice, AdvertisementData]]: List of found cubes.
+            List[CubeInfo]: List of found cubes.
         """
         return await self._scan(address=address, sort=sort, timeout=timeout)
 
@@ -140,7 +140,7 @@ class UniversalBleScanner(ScannerInterface):
             timeout (float, optional): Scan timeout. Defaults to DEFAULT_SCAN_TIMEOUT.
 
         Returns:
-            List[Tuple[BLEDevice, AdvertisementData]]: List of found cubes.
+            List[CubeInfo]: List of found cubes.
         """
         from ..scanner.platform.windows_ble import get_registered_cubes
 
@@ -170,7 +170,7 @@ class UniversalBleScanner(ScannerInterface):
             timeout (float, optional): Scan timeout. Defaults to DEFAULT_SCAN_TIMEOUT.
 
         Returns:
-            List[Tuple[BLEDevice, AdvertisementData]]: List of found cubes.
+            List[CubeInfo]: List of found cubes.
         """
         from ..scanner.platform.windows_ble import get_registered_cubes
 
